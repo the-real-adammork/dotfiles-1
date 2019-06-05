c8196b585c09dd7e41f3c7d7cba2d3957e0a068a
Adam Mork <adammork@gmail.com>
2019-06-04 18:28:31 -0700

    created two battery scripts so i can have different  colors for regular and low battery. WORKING

--- tmux_scripts/low_battery.sh
+++ tmux_scripts/low_battery.sh
@@ -2,14 +2,14 @@
 
 doubleDigit=$(pmset -g batt | grep [0-9][0-9]% | awk 'NR==1{print $3}' | cut -c 1-3)
 if [ ${#doubleDigit} -gt 1 ]; then
-  if [ "$doubleDigit" -ge 20 ]; then
+  if [ "$doubleDigit" -lt 20 ]; then
 	  echo " ♥ $doubleDigit"
 	else
 	  echo ""
 	fi
 else
 	doubleDigit=$(pmset -g batt | grep [0-9]% | awk 'NR==1{print $3}' | cut -c 1-2)
-  if [ "$doubleDigit" -ge 20 ]; then
+  if [ "$doubleDigit" -lt 20 ]; then
 	  echo " ♥ $doubleDigit"
 	else
 	  echo ""
