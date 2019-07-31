b762a81d52f8ec0b574093e62b4c006a9f64813c
Adam Mork <adammork@gmail.com>
2019-07-31 14:30:23 -0700

    fix presidential approval rating emoji change

--- tmux_scripts/presidential_approval.sh
+++ tmux_scripts/presidential_approval.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 rating=$(cat /Users/adammork/.dotfiles/tmux_scripts/rcp_average.csv | grep "RCP" | awk -F "\"*,\"*" '{print $4}')
+threshold=45.0
 
-if [ ${#rating} -gt 45 ]; then
+if (( $(echo "$rating > $threshold" |bc -l) )); then
   echo "👍 $rating %"
 else
   echo "🤞 $rating %"
